22a57620c5c87a17d0edcb3d686a6d5f41dcf754
Brian Foo <brian@youaremyjoy.org>
2018-01-17 17:07:58 -0500

    forcings: add messaging

--- temperature-forcings/js/messages.js
+++ temperature-forcings/js/messages.js
@@ -8,16 +8,33 @@ var Messages = (function() {
   }
 
   Messages.prototype.init = function(){
-    this.$el = $(this.opt.el);
+    var $el = $(this.opt.el);
     this.forcings = this.opt.forcings;
+
+    var $messages = {};
+    var $wrapper = $('<div/>');
+    _.each(this.forcings, function(forcing, key){
+      var $message = $('<div id="message-'+key+'" class="message"></div>');
+      $message.append('<h3>'+forcing.title+'</h3>');
+      $message.append('<p>'+forcing.sub+'</p>');
+      $message.css({
+        background: forcing.color,
+        top: forcing.y * 100 + '%',
+        left: forcing.x * 100 + '%'
+      });
+      $wrapper.append($message);
+      $messages[key] = $message;
+    });
+    $el.append($wrapper);
+    this.$messages = $messages;
   };
 
   Messages.prototype.forcingOff = function(value){
-
+    this.$messages[value].removeClass('active');
   };
 
   Messages.prototype.forcingOn = function(value){
-
+    this.$messages[value].addClass('active');
   };
 
   return Messages;
