c4afb571ab921a477d9bb12c57738403fd336093
Brian Foo <brian@youaremyjoy.org>
2018-02-28 15:51:07 -0500

    system ocean atmosphere: add check for localhost for video loading

--- system-ocean-atmosphere/js/init-web.js
+++ system-ocean-atmosphere/js/init-web.js
@@ -15,6 +15,13 @@ $(function() {
     config.globe["geojson"] = geojson[0];
     config.colorKey["gradient"] = colorData[0];
 
+    // temporary hack: check for localhost to load video locally or remote
+    if (window.location.hostname !== "localhost") {
+      _.each(config.globe.videos, function(v, i){
+        config.globe.videos[i].url = "https://s3.amazonaws.com/brianfoo-amnh/" + v.url;
+      });
+    }
+
     console.log('Config loaded.');
     var app = new AppOceanAtmosphere(config, content, data);
   });
