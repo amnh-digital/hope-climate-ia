81b099f75242448c8b0423634eacdd4d6baf4ddc
Brian Foo <brian@youaremyjoy.org>
2018-03-15 17:47:16 -0400

    system ocean atmosphere: add physical init

---
+++ system-ocean-atmosphere/js/init-physical.js
@@ -0,0 +1,28 @@
+$(function() {
+  $.when(
+    $.getJSON("config/base.json"),
+    $.getJSON("config/physical.json"),
+    $.getJSON("content/content.json"),
+    $.getJSON("data/countries_states.geojson"),
+    $.getJSON("data/colorGradientRainbow.json")
+
+  ).done(function(baseConfig, config, content, geojson, colorData){
+    baseConfig = baseConfig[0];
+    config = _.extend({}, baseConfig, config[0]);
+    content = content[0];
+    var data = {};
+
+    config.globe["geojson"] = geojson[0];
+    config.colorKey["gradient"] = colorData[0];
+
+    // temporary hack: check for localhost to load video locally or remote
+    if (window.location.hostname !== "localhost") {
+      _.each(config.globe.videos, function(v, i){
+        config.globe.videos[i].url = "https://s3.amazonaws.com/brianfoo-amnh/" + v.url;
+      });
+    }
+
+    console.log('Config loaded.');
+    var app = new AppOceanAtmosphere(config, content, data);
+  });
+});
