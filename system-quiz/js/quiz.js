dfe1d91d9433602bcb83c60df7e3a992fa500248
brianfoo <brian@youaremyjoy.org>
2018-04-29 16:20:59 -0400

    system quiz: add finished state

--- system-quiz/js/quiz.js
+++ system-quiz/js/quiz.js
@@ -7,54 +7,73 @@ var Quiz = (function() {
     this.init();
   }
 
+  function questionToHtml(q){
+    var id = q.id;
+
+    var $question = $('<div class="quiz-question" id="'+id+'"></div>');
+
+    var html = '<div class="q">'+q.q+'</div>';
+    if (q.htmlBefore) html += q.htmlBefore;
+    html += '<div class="as">';
+    _.each(q.a, function(a){
+      var className = "a";
+      if (a.isCorrect) className += " correct";
+      var htmlBefore = a.htmlBefore || '';
+      var htmlAfter = a.htmlAfter || '';
+      html += '<div class="'+className+'">';
+        html += '<div class="label-wrapper">'+htmlBefore+'<div class="label">'+a.label+'</div>'+htmlAfter+'</div>';
+        if (a.feedback) html += '<div class="feedback">'+a.feedback+'</div>';
+      html += '</div>'
+    });
+    if (q.htmlAfter) html += q.htmlAfter;
+    html += '</div>';
+    $question.html(html);
+
+    return $question;
+  }
+
   Quiz.prototype.init = function(){
     this.$el = $(this.opt.el);
-    this.questions = this.loadQuestions(this.opt.questions);
-    this.questionCount = this.questions.length;
+    this.loadUI();
     this.reset();
   };
 
   Quiz.prototype.done = function(){
     // alert('Done!');
 
-    this.reset();
+    this.prompt('restart');
+
+    // this.reset();
   };
 
-  Quiz.prototype.loadQuestions = function(questions){
+  Quiz.prototype.loadUI = function(){
+
+    // load questions
     var $container = $('<div class="quiz-questions"></div>');
-
-    questions = _.map(questions, function(q, i){
-      var id = 'q'+i;
-      if (q.id) id = q.id;
-
-      var $question = $('<div class="quiz-question" id="'+id+'"></div>');
-
-      var html = '<div class="q">'+q.q+'</div>';
-      if (q.htmlBefore) html += q.htmlBefore;
-      html += '<div class="as">';
-      _.each(q.a, function(a){
-        var className = "a";
-        if (a.isCorrect) className += " correct";
-        var htmlBefore = a.htmlBefore || '';
-        var htmlAfter = a.htmlAfter || '';
-        html += '<div class="'+className+'">';
-          html += '<div class="label-wrapper">'+htmlBefore+'<div class="label">'+a.label+'</div>'+htmlAfter+'</div>';
-          html += '<div class="feedback">'+a.feedback+'</div>';
-        html += '</div>'
-      });
-      if (q.htmlAfter) html += q.htmlAfter;
-      html += '</div>';
-
-      $question.html(html);
+    var questions = _.map(this.opt.questions, function(q, i){
+      var $question = questionToHtml(q);
       $container.append($question);
-
-      q.id = id;
       q.$el = $question;
       return q;
     });
-
+    this.questions = questions;
+    this.questionCount = questions.length;
     this.$el.append($container);
 
+    // load prompts
+    $container = $('<div class="quiz-prompts"></div>');
+    var prompts = _.mapObject(this.opt.prompts, function(p, key){
+      var $prompt = questionToHtml(p);
+      $container.append($prompt);
+      p.$el = $prompt;
+      p.isPrompt = true;
+      return p;
+    });
+    this.prompts = prompts;
+    this.$el.append($container);
+    $('#question-count').text(this.questionCount);
+
+    // load progress
     var $progress = $('<div class="progress">Question <span id="current">1</span> of <span class="total">'+questions.length+'</span></div>');
     this.$el.append($progress);
     this.$progress = $progress.find("#current").first();
@@ -86,6 +105,11 @@ var Quiz = (function() {
   Quiz.prototype.onAnswer = function(value){
     if (this.answered) return false;
 
+    if (this.activeQuestion.isPrompt) {
+      this.onAnswerPrompt(value);
+      return false;
+    }
+
     var _this = this;
     var index = parseInt(value);
     var $el = this.activeQuestion.$el;
@@ -97,6 +121,8 @@ var Quiz = (function() {
     $answer.addClass('active');
 
     $el.addClass('answered '+className);
+
+    if (answer.isCorrect) this.correctCount += 1;
 
     // var sound = answer.isCorrect ? "correct" : "incorrect";
     // $(document).trigger("sound.play.sprite", [sound]);
@@ -111,14 +137,55 @@ var Quiz = (function() {
     }, this.opt.answerWaitMs);
   };
 
+  Quiz.prototype.onAnswerPrompt = function(value){
+    var _this = this;
+    var index = parseInt(value);
+    var $el = this.activeQuestion.$el;
+    var answer = this.activeQuestion.a[index];
+    var $answers = $el.find('.a');
+    var $answer = $($answers[index]);
+    $answer.addClass('active');
+    $el.addClass('answered');
+
+    this.answered = true;
+
+    setTimeout(function(){
+      if (answer.action) {
+        _this[answer.action]();
+      }
+      _this.answered = false;
+      $answer.removeClass('active');
+      $el.removeClass('answered');
+    }, 2000);
+  };
+
   Quiz.prototype.onResize = function(){
 
+  };
+
+  Quiz.prototype.prompt = function(key){
+    if (this.activeQuestion) {
+      this.activeQuestion.$el.removeClass('active');
+    }
+
+    this.activeQuestion = this.prompts[key];
+
+    if (this.activeQuestion.action) {
+      this[this.activeQuestion.action]();
+    }
+
+    this.activeQuestion.$el.addClass('active');
   };
 
   Quiz.prototype.reset = function(){
     // this.shuffle();
     this.currentIndex = -1;
+    this.correctCount = 0;
     this.next();
+  };
+
+  Quiz.prototype.showResults = function(){
+    $('#correct-count').text(this.correctCount);
   };
 
   Quiz.prototype.shuffle = function(){
