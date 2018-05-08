64333d8c1bc36a8f0852c7cd1c8291b6454d2073
Brian Foo <brian@youaremyjoy.org>
2018-05-08 17:23:45 -0400

    system quiz: split content into separate files

--- system-quiz/js/init-web.js
+++ system-quiz/js/init-web.js
@@ -2,12 +2,14 @@ $(function() {
   $.when(
     $.getJSON("config/base.json"),
     $.getJSON("config/web.json"),
-    $.getJSON("content/content.json")
+    $.getJSON("content/feedback.json"),
+    $.getJSON("content/prompts.json"),
+    $.getJSON("content/questions.json")
 
-  ).done(function(baseConfig, config, content){
+  ).done(function(baseConfig, config, feedback, prompts, questions){
     baseConfig = baseConfig[0];
     config = _.extend({}, baseConfig, config[0]);
-    content = content[0];
+    content = _.extend({}, feedback[0], prompts[0], questions[0]);
 
     console.log('Config loaded.');
     var app = new AppQuiz(config, content);
