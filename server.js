a30da404632b3ec6e0698d6c3ee751c17b51abce
Brian Foo <brian@youaremyjoy.org>
2018-05-23 16:27:44 -0400

    switch to express server

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-// A simple web server
-// > npm install connect serve-static
 // > node server.js 8888
 var port = 8080;
 if (process.argv.length > 2) port = parseInt(process.argv[2]);
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(port, function(){
-  console.log('Server running on '+port+'...');
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./')); //Tells the app to serve static files from ./
+
+// Listen for control config data
+app.post('/controls/config', function (req, res) {
+  var filename = './controls/config.json';
+  fs.writeFile(dataFile, req.body.data, 'utf8', function(err, data){
+    console.log('Wrote data to file');
+  });
+  res.send({
+    status: 1,
+    message: "Success"
+  });
 });
+
+app.listen(port, () => console.log('Listening on port '+port));
