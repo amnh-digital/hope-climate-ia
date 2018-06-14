abc8a7d212b8f121f3ee987c1dc0ce6b700d8750
Brian Foo <brian@youaremyjoy.org>
2018-06-14 18:18:08 -0400

    temperature forcings: remove sibling

--- temperature-forcings/js/init-physical.js
+++ temperature-forcings/js/init-physical.js
@@ -2,14 +2,14 @@ $(function() {
   $.when(
     $.getJSON("config/base.json"),
     $.getJSON("config/physical.json"),
-    $.getJSON("../temperature-forcings/config/physical.json"),
+    $.getJSON("../temperature-regions/config/physical.json"),
     $.getJSON("content/content.json"),
     $.getJSON("data/current.json")
 
   ).done(function(baseConfig, config, configSibling, content, data){
     baseConfig = baseConfig[0];
     updateColorsFromConfig(baseConfig);
-    _.extend(config[0].controls, configSibling[0].controls);
+    // _.extend(config[0].controls, configSibling[0].controls);
     config = _.extend({}, baseConfig, config[0]);
     content = content[0];
     data = data[0];
