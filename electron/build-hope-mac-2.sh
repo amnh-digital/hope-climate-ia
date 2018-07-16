02bcfd7bd813920ac2e40cb5bc91cb0cb9f683be
Brian Foo <brian@youaremyjoy.org>
2018-07-16 16:01:41 -0400

    make hope-mac-2-combined the primary hope-mac-2

--- electron/build-hope-mac-2.sh
+++ electron/build-hope-mac-2.sh
@@ -20,9 +20,11 @@ mkdir -p build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/u
 mkdir -p build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/sleepers
 mkdir -p build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/consequences-mitigation
 mkdir -p build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/consequences-cascading
+mkdir -p build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/composites
 cp -i electron/hope-mac-2/controls.json build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/controls.json
 cp -a shared/. build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/shared/
 cp -a utilities/. build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/utilities/
 cp -a sleepers/. build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/sleepers/
 cp -a consequences-mitigation/. build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/consequences-mitigation/
 cp -a consequences-cascading/. build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/consequences-cascading/
+cp -a composites/. build/mac/hope-mac-2-darwin-x64/hope-mac-2.app/Contents/Resources/app/composites/
