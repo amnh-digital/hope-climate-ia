33c711fa86b24d967c582ff7f092dbe275f1581d
Brian Foo <brian@youaremyjoy.org>
2019-02-06 13:37:03 -0500

    temperature timescales: fix process data script

--- temperature-timescales/README.md
+++ temperature-timescales/README.md
@@ -14,15 +14,14 @@
 The following lines downloads the latest monthly and annual temperature anomaly data from [NOAA](https://www.ncdc.noaa.gov/monitoring-references/faq/anomalies.php) (you can also just do this manually)
 
 ```
-wget -O data/188001-201808.csv "https://www.ncdc.noaa.gov/cag/global/time-series/globe/land_ocean/p12/12/1880-2018.csv"
+wget -O data/188001-201812.csv "https://www.ncdc.noaa.gov/cag/global/time-series/globe/land_ocean/p12/12/1880-2018.csv"
 wget -O data/1880-2018.csv "https://www.ncdc.noaa.gov/cag/global/time-series/globe/land_ocean/ytd/12/1880-2018.csv"
 ```
 
 Now process the new data
 
 ```
-cd scripts
-python process_data.py -monthly "data/188001-201808.csv" -annual "data/1880-2018.csv" -end 2018
+python scripts/process_data.py -monthly "data/188001-201812.csv" -annual "data/1880-2018.csv" -end 2018
 ```
 
 This will process the new data through 2018 and create a new `./data/current.json` file that the app will read. Locally, you can view this here: [localhost:8080/temperature-timescales](http://localhost:8080/temperature-timescales/).
--- temperature-timescales/scripts/process_data.py
+++ temperature-timescales/scripts/process_data.py
@@ -5,6 +5,7 @@ import json
 from lib import *
 import math
 import os
+from pprint import pprint
 import sys
 
 # Sources:
@@ -16,12 +17,12 @@ import sys
 # https://www.ncdc.noaa.gov/sotc/global/201613
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-monthly', dest="MONTHLY_DATA_FILE", default="../data/188001-201803.csv", help="Monthly temperature anomaly input file")
-parser.add_argument('-annual', dest="ANNUAL_DATA_FILE", default="../data/1880-2018.csv", help="Annual temperature anomaly input file")
+parser.add_argument('-monthly', dest="MONTHLY_DATA_FILE", default="data/188001-201803.csv", help="Monthly temperature anomaly input file")
+parser.add_argument('-annual', dest="ANNUAL_DATA_FILE", default="data/1880-2018.csv", help="Annual temperature anomaly input file")
 parser.add_argument('-start', dest="START_YEAR", default=1880, type=int, help="Start year")
-parser.add_argument('-end', dest="END_YEAR", default=2017, type=int, help="End year")
+parser.add_argument('-end', dest="END_YEAR", default=2018, type=int, help="End year")
 parser.add_argument('-grad', dest="GRADIENT", default="#58e0dc,#99cccc,#adada3,#d67052,#eb5229,#ff3300", help="Color gradient")
-parser.add_argument('-out', dest="OUTPUT_FILE", default="../data/current.json", help="Output file")
+parser.add_argument('-out', dest="OUTPUT_FILE", default="data/current.json", help="Output file")
 args = parser.parse_args()
 
 START_YEAR = args.START_YEAR
@@ -42,20 +43,21 @@ monthlyData = readCSV(MONTHLY_DATA_FILE)
 annualData = readCSV(ANNUAL_DATA_FILE)
 
 # sort data
-monthlyData = sorted(monthlyData, key=lambda k: k["Date"])
-annualData = sorted(annualData, key=lambda k: k["Date"])
+monthlyData = sorted(monthlyData, key=lambda k: k["Year"])
+annualData = sorted(annualData, key=lambda k: k["Year"])
 
 # convert from anomaly to absolute
 for i,d in enumerate(monthlyData):
     monthlyData[i]["Abs"] = d["Value"] + BASELINE
-    monthlyData[i]["Year"] = int(str(d["Date"])[:4])
+    monthlyData[i]["Month"] = int(str(d["Year"])[4:])
+    monthlyData[i]["Year"] = int(str(d["Year"])[:4])
 
 for i,d in enumerate(annualData):
     annualData[i]["Abs"] = d["Value"] + BASELINE
 
 # filter
 monthlyData = [d for d in monthlyData if START_YEAR <= d["Year"] <= END_YEAR]
-annualData = [d for d in annualData if START_YEAR <= d["Date"] <= END_YEAR]
+annualData = [d for d in annualData if START_YEAR <= d["Year"] <= END_YEAR]
 
 # define domain and range
 values = [d["Value"] for d in annualData]
